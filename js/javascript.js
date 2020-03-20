c6b5df76eb0ce21fc3fd81963d3078fcac69e771
BryantDavis1986 <bryant.davis@hotmail.com>
2020-03-19 21:50:34 -0700

    updated a for and while functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,65 @@
-var answer = prompt("Do you love pitbulls?");
-if (answer === 'yes') {
-    alert('Not all Heroes wear capes!!!!');
-}
-else if (answer === 'no') {
-    alert('you suck');
-}
-else if (answer === 'maybe') {
-    alert('Walk a dog and i promise you will fall in love');
-}
-else {
-    alert('Go away');
-}
 
-var answer = prompt("Will you walk a Pitbull?");
-if (answer === 'yes') {
-    alert('Beware they are needy and will follow you home');
-}
-else if (answer === 'no') {
-    alert('why are you a horrible human being');
-}
-else if (answer === 'maybe') {
-    alert('There are no maybes in life');
-}
-else {
-    alert('They will lick you to death!!!!!!');
-}
+function walk(){
+    var answer = prompt("How many dogs would you like to walk?");
+    var album = '';
+    var pic = '<img src="https://i.pinimg.com/236x/70/bd/e5/70bde558835ff0d2b29154150a2a784c--pittbulls-beautiful-dogs.jpg">';
+
+    while( answer === '' || isNaN(answer) ){
+        answer = prompt('Put in a number')
+    }
+
+    for (var i = 0 ; i < answer ; i++){
+        album = album + pic;
+    }
+
+    return album;
+};
+
+
+
+
+
+
+
+
+// if (answer === 'yes') {
+//     alert('');
+// }
+// else if (answer === 'no') {
+//     alert('you suck');
+// }
+// else if (answer === 'maybe') {
+//     alert('Walk a dog and i promise you will fall in love');
+// }
+// else {
+//     alert('Go away');
+// }
+
+
+
+
+
+//for loop starts
+
+// for (var i = 0; i < 7; i++) {
+//     idk()
+// }
+
+
+
+// while loop starts
+
+// var count = 0;
+
+// while (count < 7) {
+//     document.writeln("looping away");
+
+//     count++;
+// }
+
+
+
+
 
 
 
@@ -38,19 +73,3 @@ function myFunction() {
     }
     document.getElementById("demo").innerHTML = txt;
   }
-
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
-if (hourNow > 18) {
-    greeting = 'Good Evening!';
-} else if (hourNow > 12) {
-    greeting = 'Good Afternoon!';
-} else if (hourNow > 0) {
-    greeting = 'Good Morning!';
-} else {
-    greeting = 'Welcome!';
-}
-
-document.write('<h3>' + greeting + '</h3>');
